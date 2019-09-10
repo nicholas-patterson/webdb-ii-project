b35a5d9bee4d5ad09d709207e26acfddb694b9d6
Nicholas Patterson <nicholaspatterson36@gmail.com>
2019-09-10 19:02:42 -0400

    :rocket: some stretch complete finished
    some stretch complete finished

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -13,21 +13,11 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  db("cars")
-    .where({ id: id })
-    .then(car => {
-      res.status(200).json(car);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: `Server could not retrieve car with the id: ${id}` });
-    });
+router.get("/:id", validateCarId, (req, res) => {
+  res.status(200).json(req.car);
 });
 
-router.post("/", (req, res) => {
+router.post("/", validateCarPost, (req, res) => {
   const body = req.body;
   db("cars")
     .insert(body, "id")
@@ -50,7 +40,29 @@ router.post("/", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.put("/:id", validateCarId, validateCarPost, (req, res) => {
+  const id = req.params.id;
+  const updatedInfo = req.body;
+  db("cars")
+    .where({ id: req.params.id })
+    .update(updatedInfo)
+    .then(count => {
+      if (!count) {
+        res
+          .status(400)
+          .json({ error: `Could not find car with the ID: ${id}` });
+      } else {
+        res.status(200).json(count);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: `Server could not update car with ID: ${id}` });
+    });
+});
+
+router.delete("/:id", validateCarId, (req, res) => {
   const id = req.params.id;
   db("cars")
     .where({ id })
@@ -70,5 +82,34 @@ router.delete("/:id", (req, res) => {
       res.status(500).json({ error: "Sever could not delete car" });
     });
 });
+
+function validateCarId(req, res, next) {
+  const id = req.params.id;
+  db("cars")
+    .where({ id })
+    .first()
+    .then(car => {
+      console.log("WHAT COME BACK ?", car);
+      if (!car) {
+        res.status(400).json({ error: "Car with that ID does not exist" });
+      } else {
+        req.car = car;
+        next();
+      }
+    });
+}
+
+function validateCarPost(req, res, next) {
+  const post = req.body;
+  const mileage = req.body.mileage;
+  const vin = req.body.vin;
+  if (vin.length > 17) {
+    res.status(400).json({ error: "Vin number is too long" });
+  } else if (typeof vin !== "string") {
+    res.status(400).json({ error: "Vin number must be a string" });
+  } else if (typeof mileage !== "number") {
+    res.status(400).json({ error: "Mileage must be a number" });
+  }
+}
 
 module.exports = router;
